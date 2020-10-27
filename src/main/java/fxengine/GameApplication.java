466c77f376d9d9accf42a96915bfb1bd4d5e9124
Alexis Liaud <liaud1u@etu.univ-lorraine.fr>
2020-10-27 14:37:14 +0100

    Commentaires.

--- src/main/java/fxengine/GameApplication.java
+++ src/main/java/fxengine/GameApplication.java
@@ -1,6 +1,5 @@
 package fxengine;
 
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
@@ -15,34 +14,68 @@ import model.PacmanController;
 import model.PacmanGame;
 import model.PacmanPainter;
 
+/**
+ * @author LIAUD Alexis
+ */
 public class GameApplication extends Application {
+    /**
+     * Stage principal du jeu
+     */
     private Stage primaryStage;
-    private GamePainter painter;
-    private GameController controller;
-    private Canvas mainCanva;
-    private Game game;
-    private Timeline gameloop;
 
-    public void run(){
+    /**
+     * Painter du jeu permettant le rafraichissement
+     */
+    private GamePainter painter;
+
+    /**
+     * Controlleur du jeu permettant de récupérer les différentes actions
+     */
+    private GameController controller;
+
+    /**
+     * Image principale du jeu
+     */
+    private Canvas mainCanva;
+
+    /**
+     * Modele
+     */
+    private Game game;
+
+    /**
+     * Lance l'execution de l'application
+     */
+    public void run() {
         launch();
     }
 
+    /**
+     * Fonction permettant de créer les scenes, de lier les event handler & contenant la boucle prinicpale.
+     *
+     * @param primaryStage Stage principal du jeu, créée lors du launch().
+     * @throws Exception
+     */
     public void start(Stage primaryStage) throws Exception {
-        this.primaryStage=primaryStage;
+        this.primaryStage = primaryStage;
 
-        Group group = new Group();
-
+        // Création du jeu
         game = new PacmanGame("helpFilePacman.txt");
 
-        mainCanva = new Canvas(512,512);
 
+        // Création d'un groupe pour les canvas et ajout des canvas au groupe
+        Group group = new Group();
+        mainCanva = new Canvas(512, 512);
         group.getChildren().add(mainCanva);
 
+        // Création de la vue et du controlleur
         painter = new PacmanPainter(mainCanva);
-        controller=new PacmanController();
+        controller = new PacmanController();
 
+        // Premier affichage
         painter.draw();
 
+        // Actions à effectuer lors de la fermeture de l'application
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             public void handle(WindowEvent event) {
                 System.out.println("Fermeture");
@@ -51,8 +84,10 @@ public class GameApplication extends Application {
             }
         });
 
+        // Création de la scene principale contenant le groupe de canvas
         Scene scene = new Scene(group,primaryStage.getWidth(),primaryStage.getHeight(), Color.WHITESMOKE);
 
+        // Actions à effectuer lors d'un click sur une touche
         scene.setOnKeyPressed(
                 new EventHandler<KeyEvent>() {
                     public void handle(KeyEvent event) {
@@ -61,6 +96,7 @@ public class GameApplication extends Application {
                 }
         );
 
+        // Actions à effectuer lors du relâchement d'une touche
         scene.setOnKeyReleased(
                 new EventHandler<KeyEvent>() {
                     public void handle(KeyEvent event) {
@@ -69,14 +105,12 @@ public class GameApplication extends Application {
                 }
         );
 
-        final long start = System.nanoTime();
-
-
+        // Création et lancement de la boucle principale de gameplay
         GameLoop loop = new GameLoop(painter,controller,game);
         loop.start();
 
+        // Paramètrage et affichage de la fenêtre principale
         primaryStage.setScene(scene);
-        primaryStage.setFullScreen(true);
         primaryStage.setTitle("Pacman");
         primaryStage.show();
     }
--- src/main/java/fxengine/GameLoop.java
+++ src/main/java/fxengine/GameLoop.java
@@ -2,10 +2,25 @@ package fxengine;
 
 import javafx.animation.AnimationTimer;
 
+/**
+ * @author LIAUD Alexis
+ */
 public class GameLoop extends AnimationTimer {
+    /**
+     * Vue du jeu
+     */
     private final GamePainter painter;
+
+    /**
+     * Controlleur du jeu
+     */
     private final GameController controller;
+
+    /**
+     * Jeu
+     */
     private final Game game;
+
 
     public GameLoop(GamePainter painter, GameController controller, Game game) {
         this.painter = painter;
@@ -13,9 +28,14 @@ public class GameLoop extends AnimationTimer {
         this.game = game;
     }
 
+    /**
+     * A chaque frame, mise à jour de la vue et évolution de l'état du jeu
+     *
+     * @param now
+     */
     public void handle(long now) {
-                painter.draw();
-                game.evolve(controller.getCommand());
-        }
+        painter.draw();
+        game.evolve(controller.getCommand());
+    }
 
 }
