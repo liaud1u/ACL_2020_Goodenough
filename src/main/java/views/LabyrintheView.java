bd00165bdb9dc041819bf4038c353c80a2d0b19f
Florian Poirel <florian.poirel54330@gmail.com>
2020-11-03 23:03:11 +0100

    LabyVue

--- src/main/java/views/LabyrintheView.java
+++ src/main/java/views/LabyrintheView.java
@@ -13,18 +13,17 @@ public class LabyrintheView extends Group {
 
 
     public LabyrintheView(Labyrinthe l) {
-        Case[][] plateau = l.getLabyrinthe();
+        char[][] plateau = l.getLabyrintheGUI();
 
         for (int i = 0; i < plateau.length; i++) {
             for (int j = 0; j < plateau.length; j++) {
                   System.out.println(plateau[i][j]);
             }
-
         }
         System.out.println(plateau);
-        for (Case[] cc : plateau) {
-            for (Case c : cc) {
-                this.getChildren().add(new CaseView(c));
+        for (char[] cc : plateau) {
+            for (char c : cc) {
+                //this.getChildren().add(new CaseView(c));
             }
         }
     }
