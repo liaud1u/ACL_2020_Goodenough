35772ff82fe06455a2fc4ee045c9566f65d9afb9
Ribeyrolles Matthieu <ribeyrolles.matthieu@gmail.com>
2020-11-03 19:10:45 +0100

    Bloc size relative to scene: start

--- src/main/java/fxengine/GameApplication.java
+++ src/main/java/fxengine/GameApplication.java
@@ -2,6 +2,8 @@ package fxengine;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -17,110 +19,116 @@ import model.PacmanPainter;
  * @author LIAUD Alexis
  */
 public class GameApplication extends Application {
+  public static DoubleProperty blocSize = new SimpleDoubleProperty();
 
-    /**
-     * Hauteur de la fenêtre
-     */
-    private static final int HEIGHT = 600;
+  /**
+   * Hauteur de la fenêtre
+   */
+  private static final int HEIGHT = 600;
 
-    /**
-     * Largeur de la fenêtre
-     */
-    private static final int WIDTH = 800;
+  /**
+   * Largeur de la fenêtre
+   */
+  private static final int WIDTH = 800;
 
-    /**
-     * Stage principal du jeu
-     */
-    private Stage primaryStage;
+  /**
+   * Stage principal du jeu
+   */
+  private Stage primaryStage;
 
-    /**
-     * Painter du jeu permettant le rafraichissement
-     */
-    private GamePainter painter;
+  /**
+   * Painter du jeu permettant le rafraichissement
+   */
+  private GamePainter painter;
 
-    /**
-     * Controlleur du jeu permettant de récupérer les différentes actions
-     */
-    private GameController controller;
+  /**
+   * Controlleur du jeu permettant de récupérer les différentes actions
+   */
+  private GameController controller;
 
-    /**
-     * Image principale du jeu
-     */
-    private Group root;
+  /**
+   * Image principale du jeu
+   */
+  private Group root;
 
-    /**
-     * Modele
-     */
-    private PacmanGame game;
+  /**
+   * Modele
+   */
+  private PacmanGame game;
 
-    /**
-     * Lance l'execution de l'application
-     */
-    public void run() {
-        launch();
-    }
+  /**
+   * Lance l'execution de l'application
+   */
+  public void run() {
+    launch();
+  }
 
-    /**
-     * Fonction permettant de créer les scenes, de lier les event handler & contenant la boucle prinicpale.
-     *
-     * @param primaryStage Stage principal du jeu, créée lors du launch().
-     * @throws Exception
-     */
-    public void start(Stage primaryStage) throws Exception {
-        this.primaryStage = primaryStage;
+  /**
+   * Fonction permettant de créer les scenes, de lier les event handler & contenant la boucle prinicpale.
+   *
+   * @param primaryStage Stage principal du jeu, créée lors du launch().
+   * @throws Exception
+   */
+  public void start(Stage primaryStage) throws Exception {
+    this.primaryStage = primaryStage;
 
-        // Création du jeu
-        game = new PacmanGame("helpFilePacman.txt");
+    // Création du jeu
+    game = new PacmanGame("helpFilePacman.txt");
 
 
-        // Création d'un groupe pour les canvas et ajout des canvas au groupe
-        root = new Group();
+    // Création d'un groupe pour les canvas et ajout des canvas au groupe
+    root = new Group();
 
-        // Création de la vue et du controlleur
-        painter = new PacmanPainter(root, game);
-        controller = new PacmanController();
+    // Création de la vue et du controlleur
+    painter = new PacmanPainter(root, game);
+    controller = new PacmanController();
 
-        // Premier affichage
-        painter.draw();
+    // Premier affichage
+    painter.draw();
 
-        // Actions à effectuer lors de la fermeture de l'application
-        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            public void handle(WindowEvent event) {
-                System.out.println("Fermeture");
-                Platform.exit();
-                System.exit(0);
-            }
-        });
+    // Actions à effectuer lors de la fermeture de l'application
+    primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+      public void handle(WindowEvent event) {
+        System.out.println("Fermeture");
+        Platform.exit();
+        System.exit(0);
+      }
+    });
 
-        // Création de la scene principale contenant le groupe de canvas
-        Scene scene = new Scene(root, WIDTH, HEIGHT, Color.WHITESMOKE);
+    // Création de la scene principale contenant le groupe de canvas
+    Scene scene = new Scene(root, WIDTH, HEIGHT, Color.BLACK);
 
-        // Actions à effectuer lors d'un click sur une touche
-        scene.setOnKeyPressed(
-                new EventHandler<KeyEvent>() {
-                    public void handle(KeyEvent event) {
-                        controller.keyPressed(event);
-                    }
-                }
-        );
+    blocSize.bind(scene.widthProperty());
 
-        // Actions à effectuer lors du relâchement d'une touche
-        scene.setOnKeyReleased(
-                new EventHandler<KeyEvent>() {
-                    public void handle(KeyEvent event) {
-                        controller.keyReleased(event);
-                    }
-                }
-        );
+    scene.widthProperty().addListener((a, o, n) -> {
+      System.out.println(blocSize.get() + " " + n);
+    });
+    // Actions à effectuer lors d'un click sur une touche
+    scene.setOnKeyPressed(
+      new EventHandler<KeyEvent>() {
+        public void handle(KeyEvent event) {
+          controller.keyPressed(event);
+        }
+      }
+    );
 
-        // Création et lancement de la boucle principale de gameplay
-        GameLoop loop = new GameLoop(painter,controller,game);
-        loop.start();
+    // Actions à effectuer lors du relâchement d'une touche
+    scene.setOnKeyReleased(
+      new EventHandler<KeyEvent>() {
+        public void handle(KeyEvent event) {
+          controller.keyReleased(event);
+        }
+      }
+    );
 
-        // Paramètrage et affichage de la fenêtre principale
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Pacman");
-        primaryStage.sizeToScene();
-        primaryStage.show();
-    }
+    // Création et lancement de la boucle principale de gameplay
+    GameLoop loop = new GameLoop(painter,controller,game);
+    loop.start();
+
+    // Paramètrage et affichage de la fenêtre principale
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("Pacman");
+    primaryStage.sizeToScene();
+    primaryStage.show();
+  }
 }
