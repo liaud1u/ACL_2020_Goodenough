b1e02d779bd7172fa068da1fe1c4b0df8ba1f058
Florian Poirel <florian.poirel54330@gmail.com>
2020-12-01 23:23:53 +0100

    tests

--- src/test/java/model/labyrinthe/dijkstra/DijkstraTest.java
+++ src/test/java/model/labyrinthe/dijkstra/DijkstraTest.java
@@ -36,6 +36,26 @@ class DijkstraTest {
         Dijkstra dijkstra = new Dijkstra(labyrinthe,c1);
 
         ArrayList<Case> chemin = dijkstra.getChemin(c1,c2);
+        assertNotNull(chemin);
 
+        for (int i = 0; i < chemin.size(); i++) {
+            assertNotNull(chemin.get(i));
+        }
     }
+
+    @Test
+    void getChemin2CasesIdentiques() {
+        //Case où spawn le pacman donc n'est jamais un mur
+        Case c1 = new Case(1,1);
+
+        Dijkstra dijkstra = new Dijkstra(labyrinthe,c1);
+
+        ArrayList<Case> chemin = dijkstra.getChemin(c1,c1);
+        assertNotNull(chemin);
+
+        for (int i = 0; i < chemin.size(); i++) {
+            assertNotNull(chemin.get(i));
+        }
+    }
+
 }
