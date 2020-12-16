6762d74229a83b2d88d7aa029c123c4fc946452c
Alexis LIAUD <liaud1u@etu.univ-lorraine.fr>
2020-12-16 15:23:19 +0100

    RandomMovementStrategy tests.

--- src/test/java/model/movementstrategy/RandomMovementStrategyTest.java
+++ src/test/java/model/movementstrategy/RandomMovementStrategyTest.java
@@ -1,20 +1,102 @@
 package model.movementstrategy;
 
+import fxengine.Game;
+import model.Direction;
+import model.PacmanGame;
+import model.labyrinthe.Case;
+import model.labyrinthe.Labyrinthe;
+import model.monster.GhostType;
+import model.monster.Monster;
+import model.util.RandomGenerator;
+import model.util.Util;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
 
 class RandomMovementStrategyTest {
 
+    private RandomMovementStrategy strategy;
+    private Monster monstre;
+    private PacmanGame game;
+
     @BeforeEach
     void setUp() {
+        game = new PacmanGame("");
+        monstre=new Monster((PacmanGame) game,2,2, GhostType.RED);
+        strategy = new RandomMovementStrategy(monstre,((PacmanGame) game).getLabyrinthe());
     }
 
     @Test
-    void move() {
+    void moveInitUp() {
+        Direction direction = Direction.UP;
+
+        int oldx = monstre.getX();
+        int oldy = monstre.getY();
+
+        strategy.setDirection(direction);
+        strategy.move();
+
+        assertEquals(oldx+strategy.getDirection().getX_dir(),monstre.getX());
+        assertEquals(oldy+ strategy.getDirection().getY_dir(),monstre.getY());
+
+    }
+
+    @Test
+    void moveInitDown() {
+        Direction direction = Direction.DOWN;
+
+        int oldx = monstre.getX();
+        int oldy = monstre.getY();
+
+        strategy.setDirection(direction);
+        strategy.move();
+
+        assertEquals(oldx+strategy.getDirection().getX_dir(),monstre.getX());
+        assertEquals(oldy+ strategy.getDirection().getY_dir(),monstre.getY());
+
+    }
+
+    @Test
+    void moveInitRight() {
+        Direction direction = Direction.RIGHT;
+
+        int oldx = monstre.getX();
+        int oldy = monstre.getY();
+
+        strategy.setDirection(direction);
+        strategy.move();
+
+        assertEquals(oldx+strategy.getDirection().getX_dir(),monstre.getX());
+        assertEquals(oldy+ strategy.getDirection().getY_dir(),monstre.getY());
+
+    }
+
+    @Test
+    void moveInitLeft() {
+        Direction direction = Direction.LEFT;
+
+        int oldx = monstre.getX();
+        int oldy = monstre.getY();
+
+        strategy.setDirection(direction);
+        strategy.move();
+
+        assertEquals(oldx+strategy.getDirection().getX_dir(),monstre.getX());
+        assertEquals(oldy+ strategy.getDirection().getY_dir(),monstre.getY());
+
     }
 
     @Test
     void chooseRandomDirection() {
+            Direction previousDirectionTest = strategy.getDirection();
+
+            strategy.chooseDirection();
+
+            assertNotEquals(previousDirectionTest, strategy.getDirection().opposite());
+            assertFalse(game.getLabyrinthe().getCaseLabyrinthe(monstre.getX() + strategy.getDirection().getX_dir(), monstre.getY() + strategy.getDirection().getY_dir()).estUnMur());
+            assertTrue(game.getLabyrinthe().getCaseLabyrinthe(monstre.getX() + strategy.getDirection().getX_dir(), monstre.getY() + strategy.getDirection().getY_dir()).estVide());
     }
 
 }
---
+++ src/test/java/model/movementstrategy/RespawnMovementStrategyTest.java
@@ -0,0 +1,21 @@
+package model.movementstrategy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RespawnMovementStrategyTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void chooseDirection() {
+    }
+}
